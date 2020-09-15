67bb546cc337b5cc62c913aee4cfca0e76dc1da8
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-09-15 13:49:38 -0500

    created Homepage Component

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+    return ( <
+        div className = "jumbotron" >
+        <
+        h1 > Pluralsight Administration < /h1>    <
+        p > React, Flux and React router
+        for ultra responsive Web App. < /p>    <
+        /div>
+    );
+}
+
+export default HomePage;
