5964f0b57f0b95b4d827e8b6c6559c8958e0f1a5
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-09-15 15:57:21 -0500

    Created header,homepage and aboutpage components

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 function HomePage() {
-    return ( <
-        div className = "jumbotron" >
-        <
-        h1 > Pluralsight Administration < /h1>    <
-        p > React, Flux and React router
-        for ultra responsive Web App. < /p>    <
-        /div>
-    );
+  return (
+    <div className="jumbotron">
+      <h1> Pluralsight Administration </h1>
+      <p> React, Flux and React router for ultra responsive Web App. </p>
+      <a href="/about">About</a>
+    </div>
+  );
 }
 
 export default HomePage;
